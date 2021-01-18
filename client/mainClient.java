e4459abde4dc9fc9a0561487f54e716a8fa072ff
chanelle740 <hirwa.chanelle@gmail.com>
2021-01-18 18:24:25 +0200

    basic client server chat

---
+++ client/mainClient.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.net.Socket;
+public class mainClient {
+
+	public static void main(String[] args) throws Exception{
+		
+		Socket socket = new Socket("localhost",8094);
+		DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+		DataInputStream input = new DataInputStream(socket.getInputStream());
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String request = " ";
+		String response = " ";
+		
+		while(!request.equals("end")) {
+			System.out.println("Write a request to the server: ");
+			request = reader.readLine();
+			output.writeUTF(request);
+			output.flush();
+			response = input.readUTF();
+			System.out.println("Response from the server: " +response);
+		}
+		socket.close();
+	}
+
+}
---
+++ server/mainServer.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class mainServer {
+
+	public static void main(String[] args) throws Exception{
+		
+		ServerSocket serverSocket = new ServerSocket(8094);
+		
+		  System.out.println("Server started");
+		  System.out.println("Waiting for a client ...");
+		  
+		 Socket socket = serverSocket.accept();
+		 System.out.println("Client accepted"); 
+		 
+		 DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+		 DataInputStream input = new DataInputStream(socket.getInputStream());
+		 
+		 BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		 String request = " ";
+		 String response = " ";
+			
+		while(!request.equals("end")) {
+			request = input.readUTF();
+			System.out.println("Client request : " + request);
+			System.out.println("Write response : " + response);
+			response = reader.readLine();
+			output.writeUTF(response);
+			System.out.println("Server response : " + response);
+			
+			
+		}
+		input.close();
+		serverSocket.close();
+		
+	}
+
+}
