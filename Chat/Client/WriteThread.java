3b9cd5bff1d174dbf5eef95230993de284f9e27c
Didien Munezero <51176044+didiermunezer38@users.noreply.github.com>
2021-01-19 06:36:17 +0200

    Update WriteThread.java

--- Chat/Client/WriteThread.java
+++ Chat/Client/WriteThread.java
@@ -6,8 +6,6 @@ import java.net.*;
  * This thread is responsible for reading user's input and send it
  * to the server.
  * It runs in an infinite loop until the user types 'bye' to quit.
- *
- * @author www.codejava.net
  */
 public class WriteThread extends Thread {
     private PrintWriter writer;
