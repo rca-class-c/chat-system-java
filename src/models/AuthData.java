0fe5c6c27e3ef86a0bde66af6b3357e91063871e
didiermunezer38 <didiermunezer38@gmail.com>
2021-01-28 08:41:37 +0200

    basic auth data switch

--- src/models/AuthData.java
+++ src/models/AuthData.java
@@ -1,6 +1,37 @@
 package models;
 
+import java.util.Objects;
+
 public class AuthData {
     public models.User user;
     public String token;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof AuthData)) return false;
+        AuthData authData = (AuthData) o;
+        return getUser().equals(authData.getUser()) && getToken().equals(authData.getToken());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getUser(), getToken());
+    }
 }
