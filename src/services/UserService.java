3fabdf09281112a4a568a688596116318412a5d2
didiermunezer38 <didiermunezer38@gmail.com>
2021-01-29 17:20:35 +0200

    adding login data model and service

--- src/services/UserService.java
+++ src/services/UserService.java
@@ -1,6 +1,7 @@
 package services;
 
 import models.User;
+import models.AuthInput
 import repositories.UserRepository;
 
 import java.sql.SQLException;
@@ -9,5 +10,8 @@ public class UserService {
     private final UserRepository userRepository = new UserRepository();
     public User saveUser(User user) throws SQLException {
         return userRepository.save(user);
+    }
+    public User loginUser(AuthInput input) throws SQLException{
+        return userRepository.login(input);
     }
 }
