f3d66ea864d3895464e039dcaee80dcfa5696de7
Ntwari Clarance Liberiste <ntwaricliberi@gmail.com>
2021-02-03 20:41:06 +0200

    generating token

--- src/controllers/Token.java
+++ src/controllers/Token.java
@@ -1,6 +1,12 @@
 package controllers;
 
+import io.jsonwebtoken.Jwts;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Random;
 import java.util.TreeMap;
+import java.util.Date;
 
 public class Token {
 
@@ -18,5 +24,23 @@ public class Token {
     public void setPayload(TreeMap<String, String> payload) {
         this.payload = payload;
     }
+
+
+    public String generateToken(){
+        String jwtToken;
+        Instant now = Instant.now();
+
+        jwtToken = Jwts.builder()
+                .setSubject("Ntwari liberi")
+                .setAudience("Auth")
+                .claim("name","liberi")
+                .claim("age",new Random().nextInt(20)+1)
+                .setIssuedAt(Date.from(now))
+                .setExpiration(Date.from(now.plus(1, ChronoUnit.DAYS)))
+                .compact();
+
+        return jwtToken;
+    }
+
 }
 
