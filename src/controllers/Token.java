f8afaa2e9c6fe3239cd0c7f43e9d58d4e7c4c930
Ntwari Clarance Liberiste <ntwaricliberi@gmail.com>
2021-02-03 23:13:34 +0200

    documentation

--- src/controllers/Token.java
+++ src/controllers/Token.java
@@ -5,15 +5,29 @@ import io.jsonwebtoken.security.Keys;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
 import java.util.Base64;
 import java.util.Random;
 import java.util.TreeMap;
 import java.util.Date;
 
+
+/**
+ * Token controller
+ *
+ * @author Ntwari Clarance Liberiste
+ * @since 1.0
+ */
 public class Token {
 
     private TreeMap<String,String> payload;
 
+
+    /**
+     * Token controller
+     *
+     * @param payload Data to be included in the jwt token
+     */
     public Token(TreeMap<String,String> payload){
         this.payload = payload;
     }
@@ -28,7 +42,17 @@ public class Token {
     }
 
 
-    public String generateToken(){
+    /**
+     * Generates jwt based token basing on given data in class initialisation,
+     * given data in initialization will be used as claims so they will appear
+     * in the token
+     *
+     * @param timeToLast Time that it will take for token to be active, of type long
+     * @param unit Unit of the time, of java.time.temporal.TemporalUnit type
+     *
+     * @return token, of String type
+     */
+    public String generateToken(long timeToLast, TemporalUnit unit){
         String jwtToken;
         Instant now = Instant.now();
         String secreteKey = "V3uZKw7RPYW85FNTwiqm8JjZ9ep/jn2bZ8xy0vl8S+1OmHjJoCSNbXLl+hb8ulmnUTkxaPtBcYKM/60PjoMyPw==";
@@ -40,7 +64,7 @@ public class Token {
                 .claim("name","liberi")
                 .claim("age",new Random().nextInt(20)+1)
                 .setIssuedAt(Date.from(now))
-                .setExpiration(Date.from(now.plus(1, ChronoUnit.DAYS)))
+                .setExpiration(Date.from(now.plus(timeToLast, unit)))
                 .signWith(Keys.hmacShaKeyFor(secrete))
                 .compact();
 
