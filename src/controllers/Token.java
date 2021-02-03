fd3bdd6ec01474b3564936e454e38b5852752cfc
Ntwari Clarance Liberiste <ntwaricliberi@gmail.com>
2021-02-03 20:55:32 +0200

    sign secrete Key to token, so that it can be more secured

--- src/controllers/Token.java
+++ src/controllers/Token.java
@@ -1,9 +1,11 @@
 package controllers;
 
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.util.Base64;
 import java.util.Random;
 import java.util.TreeMap;
 import java.util.Date;
@@ -29,6 +31,8 @@ public class Token {
     public String generateToken(){
         String jwtToken;
         Instant now = Instant.now();
+        String secreteKey = "V3uZKw7RPYW85FNTwiqm8JjZ9ep/jn2bZ8xy0vl8S+1OmHjJoCSNbXLl+hb8ulmnUTkxaPtBcYKM/60PjoMyPw==";
+        byte[] secrete  = Base64.getDecoder().decode(secreteKey);
 
         jwtToken = Jwts.builder()
                 .setSubject("Ntwari liberi")
@@ -37,6 +41,7 @@ public class Token {
                 .claim("age",new Random().nextInt(20)+1)
                 .setIssuedAt(Date.from(now))
                 .setExpiration(Date.from(now.plus(1, ChronoUnit.DAYS)))
+                .signWith(Keys.hmacShaKeyFor(secrete))
                 .compact();
 
         return jwtToken;
