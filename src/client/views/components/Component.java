6d4d7b0abf2d2b0bdb3317861e70cf1d647af5d5
damour <tuyishimejeand88@gmail.com>
2021-02-05 12:15:07 +0200

    adding admin user interface and also adding bye screen to  exit functionlity

--- src/client/views/components/Component.java
+++ src/client/views/components/Component.java
@@ -4,6 +4,7 @@ import utils.CommonUtil;
 import utils.ConsoleColor;
 
 import java.util.Locale;
+import java.util.Properties;
 
 public class Component {
     public static void pageTitleView(String title) {
@@ -12,8 +13,38 @@ public class Component {
         CommonUtil.useColor(ConsoleColor.BoldColor.GREEN_BOLD);
         CommonUtil.addTabs(20, true);
         CommonUtil.useColor(ConsoleColor.UnderlineColor.GREEN_UNDERLINED);
-
         System.out.println(title);
         CommonUtil.resetColor();
+    }
+
+    /**
+     * @author damour
+     * printing  the bye key word in when person leave the sytem
+     */
+    public static void byeBye(){
+        CommonUtil.addTabs(10, true);
+        System.out.print(" ************************************************************");
+        CommonUtil.addTabs(10, true);
+        System.out.print("*                        ");
+        CommonUtil.useColor(ConsoleColor.RegularColor.YELLOW);
+        System.out.print("CHAT SYSTEM");
+        CommonUtil.resetColor();
+        System.out.print("                           *");
+        CommonUtil.addTabs(10, true);
+        System.out.print("*                                                              *");
+        CommonUtil.addTabs(10, true);
+        System.out.print("*                                                              *");
+        CommonUtil.addTabs(10, true);
+        System.out.print("*                                                              *");
+        CommonUtil.addTabs(10, true);
+        System.out.print("*                                                              *");
+        CommonUtil.addTabs(10, true);
+        System.out.print("*                                                              *");
+        CommonUtil.addTabs(10, true);
+        System.out.print("*                                                              *");
+        CommonUtil.addTabs(10, true);
+        System.out.println("  *************************************************************");
+        CommonUtil.addTabs(10, true);
+
     }
 }
