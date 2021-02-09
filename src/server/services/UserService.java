595ce5d05da65a33652b615cd4d74458b4f33031
Ntwari Clarance Liberiste <ntwaricliberi@gmail.com>
2021-02-09 21:19:50 +0200

    documenting services

--- src/server/services/UserService.java
+++ src/server/services/UserService.java
@@ -38,12 +38,42 @@ public class UserService {
     public int deleteUser(int userId) throws SQLException{
         return userRepository.deleteUser(userId);
     }
+
+    /**
+     * changing user password
+     *
+     * @param userId user id
+     * @param password user new proposed password
+     * @return true when password is updated, false when password not updated
+     * @throws SQLException throws sql exception for any error
+     * @author Ntwari Clarance Liberiste
+     */
     public boolean changePasswordById(int userId, String password) throws SQLException{
         return userRepository.changePasswordById(userId, password);
     }
+
+    /**
+     *  changing user password
+     *
+     * @param email user email
+     * @param password new user proposed password
+     * @return true when password is updated, false when password not updated
+     * @throws SQLException throws sql exception for any error
+     * @author Ntwari Clarance Liberiste
+     */
     public boolean changePasswordByEmail(String email,String password) throws SQLException{
         return userRepository.changePasswordByEmail(email,password);
     }
+
+    /**
+     * changing user password
+     *
+     * @param username user username
+     * @param password new user proposed password
+     * @return true when password is updated, false when password not updated
+     * @throws SQLException throws sql exception for any error
+     * @author Ntwari Clarance Liberiste
+     */
     public boolean changePasswordByUsername(String username, String password) throws SQLException{
         return userRepository.changePasswordByUsername(username, password);
     }
