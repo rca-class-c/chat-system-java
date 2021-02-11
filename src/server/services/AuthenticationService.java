4a4b3554edbfc82e46f692224c233ba3ff2fc320
Ntwari Clarance Liberiste <ntwaricliberi@gmail.com>
2021-02-11 12:03:34 +0200

    access token

--- src/server/services/AuthenticationService.java
+++ src/server/services/AuthenticationService.java
@@ -1,4 +1,14 @@
 package server.services;
 
 public class AuthenticationService {
+    private String token;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
 }
