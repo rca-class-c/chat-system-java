f533cc32054042d7524b331e2321d87e4a37af49
Ntwari Clarance Liberiste <ntwaricliberi@gmail.com>
2021-02-20 10:16:49 +0200

    loading Redis connection url from db.properties

--- src/server/config/JedisConfig.java
+++ src/server/config/JedisConfig.java
@@ -1,11 +1,32 @@
 package server.config;
 import redis.clients.jedis.Jedis;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.DriverManager;
+import java.util.Properties;
+
 public class JedisConfig {
 
 	public Jedis conn() {
-		Jedis jedis = new Jedis("localhost");
-	     return jedis;
+		Jedis conn = null;
+
+		try (FileInputStream f = new FileInputStream("src/server/config/db.properties")) {
+
+			// load the properties file
+			Properties pros = new Properties();
+			pros.load(f);
+
+			// assign migrations.sql parameters
+			String url = pros.getProperty("REDIS_URL");
+
+			conn = new Jedis(url);
+
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+
+	     return conn;
 	}
 	
 
