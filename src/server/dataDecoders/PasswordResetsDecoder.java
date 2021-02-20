b3c791ead7bbfc847f8852fa02a2e5c667056aff
Ntwari Clarance Liberiste <ntwaricliberi@gmail.com>
2021-02-20 21:37:08 +0200

    adding reset Password Decode
    Took 25 minutes

--- src/server/dataDecoders/PasswordResetsDecoder.java
+++ src/server/dataDecoders/PasswordResetsDecoder.java
@@ -5,6 +5,9 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import server.models.PasswordResets;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Password Reset Decoder
  *
@@ -28,5 +31,25 @@ public class PasswordResetsDecoder {
         JsonNode dataDecrypt = objectMapper.readTree(data);
         return new PasswordResets(dataDecrypt.get("passwordResetEmail").asText(),dataDecrypt.get("passwordResetExpiryDate").asText());
     }
+
+    /**
+     * Reset Password decoder
+     *
+     * @return Map object with userEmail,otp,newPassword
+     * @throws JsonProcessingException throws Json Processing Exception
+     */
+    public Map<String,String> resetPasswordDecode() throws JsonProcessingException  {
+        Map<String, String> myObject = new HashMap<String,String>();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode dataDecrypt = objectMapper.readTree(data);
+
+        myObject.putIfAbsent("userEmail",dataDecrypt.get("userEmail").asText());
+        myObject.putIfAbsent("otp",dataDecrypt.get("userEmail").asText());
+        myObject.putIfAbsent("newPassword",dataDecrypt.get("userEmail").asText());
+
+        return myObject;
+    }
+
 
 }
