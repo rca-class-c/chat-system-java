163910deed58da243bfc352ddb38c301733aa9b4
Ntwari Clarance Liberiste <ntwaricliberi@gmail.com>
2021-02-20 20:08:09 +0200

    documenting class
    Took 2 minutes

--- src/server/dataDecoders/PasswordResetsDecoder.java
+++ src/server/dataDecoders/PasswordResetsDecoder.java
@@ -5,12 +5,24 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import server.models.PasswordResets;
 
+/**
+ * Password Reset Decoder
+ *
+ * @author Ntwari Clarance Liberiste
+ * @since 1.0
+ */
 public class PasswordResetsDecoder {
     String data;
     public PasswordResetsDecoder(String data){
         this.data = data;
     };
 
+    /**
+     *password resets decoder
+     * @return PasswordResets model
+     * @throws JsonProcessingException a json proccessor exception
+     * @author Ntwari Clarance Liberiste
+     */
     public PasswordResets initiatePasswordResetDecode() throws JsonProcessingException {
         ObjectMapper objectMapper = new ObjectMapper();
         JsonNode dataDecrypt = objectMapper.readTree(data);
