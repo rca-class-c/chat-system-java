aeafba961f03d46480a87c0d289b73cc91d9a8f6
Ntwari Clarance Liberiste <ntwaricliberi@gmail.com>
2021-02-20 19:38:47 +0200

    creating password reset initiation handler
    Took 5 minutes

--- src/server/requestHandlers/PasswordResetsRequestHandler.java
+++ src/server/requestHandlers/PasswordResetsRequestHandler.java
@@ -1,5 +1,36 @@
 package server.requestHandlers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.sql.SQLException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import server.dataDecoders.PasswordResetsDecoder;
+import server.models.PasswordResets;
+import server.models.Response;
+import server.services.PasswordResetService;
+
+import java.io.PrintWriter;
+
+/**
+ * password reset request handling class
+ *
+ * @author Ntwari Clarance Liberiste
+ * @since 1.0
+ */
 public class PasswordResetsRequestHandler {
 
+    public void handlePasswordResetInitiation(String data, PrintWriter writer, ObjectMapper objectMapper) throws JsonProcessingException, SQLException{
+        PasswordResets passwordReset = new PasswordResetService().create(new PasswordResetsDecoder(data).initiatePasswordResetDecode());
+
+        if(passwordReset == null){
+            System.out.println("password reset initiation failed");
+            Response response = new Response(null,false);
+            String ResponseAsString = objectMapper.writeValueAsString(response);
+            writer.println(ResponseAsString);
+        }else{
+            Response response = new Response(passwordReset,true);
+            String ResponseAsString = objectMapper.writeValueAsString(response);
+            System.out.println("password reset email was sent to " + passwordReset.getEmail()+"");
+            writer.println(ResponseAsString);
+        }
+    }
 }
