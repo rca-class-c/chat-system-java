20c96ba9284831b8c12448b207fea7f68b3b66eb
Ntwari Clarance Liberiste <ntwaricliberi@gmail.com>
2021-02-20 18:02:19 +0200

    adding initiatePasswordDecode method

--- src/server/dataDecoders/PasswordResetsDecoder.java
+++ src/server/dataDecoders/PasswordResetsDecoder.java
@@ -1,8 +1,20 @@
 package server.dataDecoders;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import server.models.PasswordResets;
+
 public class PasswordResetsDecoder {
     String data;
     public PasswordResetsDecoder(String data){
         this.data = data;
     };
+
+    public PasswordResets initiatePasswordResetDecode() throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode dataDecrypt = objectMapper.readTree(data);
+        return new PasswordResets(dataDecrypt.get("passwordResetEmail").asText(),dataDecrypt.get("passwordResetExpiryDate").asText());
+    }
+
 }
