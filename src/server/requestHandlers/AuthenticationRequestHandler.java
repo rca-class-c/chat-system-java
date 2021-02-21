c515f1b73c09e0957b6f221874d3f25ff5f107c5
Ntwari Clarance Liberiste <ntwaricliberi@gmail.com>
2021-02-21 11:22:50 +0200

    method to handle login
    Took 11 minutes

--- src/server/requestHandlers/AuthenticationRequestHandler.java
+++ src/server/requestHandlers/AuthenticationRequestHandler.java
@@ -1,5 +1,15 @@
 package server.requestHandlers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import server.dataDecoders.AuthenticationDecoder;
+import server.models.Response;
+import server.services.AuthenticationService;
+
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.Map;
+
 /**
  * Authentication requests handler
  *
@@ -8,4 +18,21 @@ package server.requestHandlers;
  */
 public class AuthenticationRequestHandler {
 
+    public void handleLogin(String data, PrintWriter writer, ObjectMapper objectMapper) throws JsonProcessingException, SQLException {
+        Map<String,String> authData = new AuthenticationDecoder(data).loginDecoder();
+
+        String authToken = new AuthenticationService().login(authData.get("username"), authData.get("password"));
+
+        if(authToken == null){
+            System.out.println("invalid credentials");
+            Response response = new Response(null,false);
+            String responseAsString = objectMapper.writeValueAsString(response);
+            writer.println(responseAsString);
+        }else{
+            System.out.println("logged in successfully");
+            Response response = new Response(authToken,false);
+            String responseAsString = objectMapper.writeValueAsString(response);
+            writer.println(responseAsString);
+        }
+    }
 }
