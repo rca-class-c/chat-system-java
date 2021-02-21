ebdbd10dc62437eab257693d45b905a3bbc146e9
Ntwari Clarance Liberiste <ntwaricliberi@gmail.com>
2021-02-21 10:54:35 +0200

    adding neccessary method for service
    Took 27 seconds

--- src/server/services/AuthenticationService.java
+++ src/server/services/AuthenticationService.java
@@ -1,5 +1,7 @@
 package server.services;
 
+import server.repositories.AuthenticationRepository;
+
 /**
  * authentication service
  *
@@ -8,4 +10,50 @@ package server.services;
  */
 public class AuthenticationService {
 
+    private final AuthenticationRepository authRepository  = new AuthenticationRepository();
+
+
+    /**
+     * Gets token.
+     *
+     * @return the token
+     * @author Ntwari Clarance Liberiste
+     */
+    public String getToken() {
+        return authRepository.getToken();
+    }
+
+    /**
+     * Sets token.
+     *
+     * @param token the token
+     * @author Ntwari Clarance Liberiste
+     */
+    public void setToken(String token) {
+        authRepository.setToken(token);
+    }
+
+
+    /**
+     * login with user credentials
+     *
+     * @param username username of the user who is going to log in
+     * @param password password of the user
+     * @return token as String or null when credentials are not verified
+     * @author Ntwari Clarance Liberiste
+     */
+    public String login(String username, String password){
+        return authRepository.login(username, password);
+    }
+
+
+    /**
+     * check if token is valid token, it may be invalid due to that it is invalid or it has expired
+     *
+     * @author Ntwari clarance Liberiste
+     * @return true when it is valid, false when it is invalid
+     */
+    public boolean isAuthTokenValid(){
+        return authRepository.isAuthTokenValid();
+    }
 }
