3445c3bc54534a7036d1ede0aaa1f84c089cc11f
Ntwari Clarance Liberiste <ntwaricliberi@gmail.com>
2021-02-21 10:58:56 +0200

    creating login decoder
    Took 2 minutes

--- src/server/dataDecoders/AuthenticationDecoder.java
+++ src/server/dataDecoders/AuthenticationDecoder.java
@@ -1,5 +1,10 @@
 package server.dataDecoders;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import server.models.PasswordResets;
+
 /**
  * Authentication Decoder
  *
@@ -11,4 +16,18 @@ public class AuthenticationDecoder {
     public AuthenticationDecoder(String data){
         this.data = data;
     };
+
+
+    /**
+     *login decoder
+     *
+     * @return PasswordResets model
+     * @throws JsonProcessingException a json proccessor exception
+     * @author Ntwari Clarance Liberiste
+     */
+    public PasswordResets loginDecoder() throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode dataDecrypt = objectMapper.readTree(data);
+        return new PasswordResets(dataDecrypt.get("username").asText(),dataDecrypt.get("password").asText());
+    }
 }
