0789407165c3c8ae7d5a1bb38918c94097ccb42c
Ntwari Clarance Liberiste <ntwaricliberi@gmail.com>
2021-02-21 11:01:32 +0200

    creating AuthenticationRequestHandler
    Took 3 minutes

---
+++ src/server/requestHandlers/AuthenticationRequestHandler.java
@@ -0,0 +1,11 @@
+package server.requestHandlers;
+
+/**
+ * Authentication requests handler
+ *
+ * @author Ntwari Clarance Liberiste
+ * @since 1.0
+ */
+public class AuthenticationRequestHandler {
+
+}
